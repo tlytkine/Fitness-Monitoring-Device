0a333a1b0a8fb0591e1206cd27ef38e0b7f39cb7
Timothy Lytkine <tlytkine@gmail.com>
2018-03-23 17:06:49 -0400

    added c and arduino skeletons

--- part1/part1.ino
+++ part1/part1.ino
@@ -1,23 +1,35 @@
-/*
- You will also write a C program to run on a UNIX host that communicates with the Arduino. The program
-will expose a command prompt that supports the following commands:
-• show X: Set the output device to show X as the current heart rate instead of the current real-time value.
-This will be useful while debugging.
-• pause: Pause the output and keep the display device showing the current reading
-• resume: Show the real-time heart rate on the display device. This should be the default mode of the
-system
-• exit: Exits the host program
- */
+
 
 
 
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  pinMode(blinkPin,OUTPUT);
+  pinMode(fadePin,OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  int input;
+  int num;
+  int buff[64];
+  int j = -1;
+  if(Serial.available()>0){
+    input = Serial.read();
 
+    if(input==',')
+    {
+      /* num = calc(); */
+      j = -1;
+      Serial.println(num);
+    }
+    else 
+    {
+      j++;
+      buff[j]=input;
+    }
+  }
 }
